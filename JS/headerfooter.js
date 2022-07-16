d827e01e8751f827eb65eba1f0e603a17d1d2154
BoYueChuang <otis29516368@gmail.com>
2022-07-16 22:55:26 +0800

    個人專案

---
+++ JS/headerfooter.js
@@ -0,0 +1,100 @@
+const btn = document.querySelector('.btn');
+    
+btn.addEventListener('click', () => {
+const contactUS = document.querySelector('#contactUS');
+
+if (contactUS.style.display === 'none') {
+    // 👇️ this SHOWS the form
+    contactUS.style.display = 'block';
+} else {
+    // 👇️ this HIDES the form
+    contactUS.style.display = 'none';
+}
+});
+
+const btn2 = document.getElementById('btn2');
+btn2.addEventListener('click', () => {
+// const contactUS = document.getElementById('contactUS');
+
+if (contactUS.style.display === 'none') {
+    // 👇️ this SHOWS the form
+    contactUS.style.display = 'block';
+} else {
+    // 👇️ this HIDES the form
+    contactUS.style.display = 'none';
+}
+});
+
+const btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', () => {
+const search = document.getElementById('search');
+
+if (search.style.display === 'none') {
+    search.style.display = 'block';
+} else {
+
+    search.style.display = 'none';
+}
+});
+
+const btn4 = document.getElementById('btn4');
+btn4.addEventListener('click', () => {
+const search = document.getElementById('search');
+
+if (search.style.display === 'none') {
+    search.style.display = 'block';
+} else {
+
+    search.style.display = 'none';
+}
+});
+
+const cart = document.getElementById('cart');
+cart.addEventListener('click', () => {
+const showCart = document.getElementById('showCart');
+
+if (showCart.style.display === 'none') {
+    showCart.style.display = 'block';
+} else {
+
+    showCart.style.display = 'none';
+}
+});
+
+const closeCart = document.getElementById('closeCart');
+closeCart.addEventListener('click', () => {
+const showCart = document.getElementById('showCart');
+
+if (showCart.style.display === 'none') {
+    showCart.style.display = 'block';
+} else {
+
+    showCart.style.display = 'none';
+}
+});
+
+const goCart = document.querySelector('.goCart');
+goCart.addEventListener('click', () => {
+const showCart = document.getElementById('showCart');
+
+if (showCart.style.display === 'none') {
+    showCart.style.display = 'block';
+} else {
+
+    showCart.style.display = 'none';
+}
+});
+
+
+const goCart1 = document.querySelector('.goCart1');
+goCart1.addEventListener('click', () => {
+const showCart = document.getElementById('showCart');
+
+if (showCart.style.display === 'none') {
+    showCart.style.display = 'block';
+} else {
+
+    showCart.style.display = 'none';
+}
+});
+
